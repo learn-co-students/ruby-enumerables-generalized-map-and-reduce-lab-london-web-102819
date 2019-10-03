f11a2e272a6a33d59a65e808f853a99302d7a389
Zoraaver Natt <zoraaver@gmail.com>
2019-10-03 12:10:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+
+def map(source_array)
+  new = []
+  j = 0
+  while j < source_array.length do
+    new[j] = yield(source_array[j])
+    j += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point = 0)
+  j = 0
+  while j < source_array.length do
+    if source_array[j]
+      yield(source_array[j])
+      starting_point = yield(source_array[j], starting_point)
+      j += 1
+      p starting_point
+    else
+      yield(source_array[j])
+      j += 1
+    end
+  end
+end
+end
+
+p reduce([1,2,3]) {|a|  a = a}
