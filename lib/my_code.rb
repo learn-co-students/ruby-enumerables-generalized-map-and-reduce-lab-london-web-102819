d5f8e3e23220d896c2af9cb42a65dbcce5c99bd5
Zoraaver Natt <zoraaver@gmail.com>
2019-10-03 12:46:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,20 +9,16 @@ def map(source_array)
   new
 end
 
+
 def reduce(source_array, starting_point = 0)
   j = 0
   while j < source_array.length do
-    if source_array[j]
-      yield(source_array[j])
       starting_point = yield(source_array[j], starting_point)
       j += 1
-      p starting_point
-    else
-      yield(source_array[j])
-      j += 1
-    end
+      puts starting_point
   end
+  if starting_point == nil
+    return false
+  end
+  starting_point
 end
-end
-
-p reduce([1,2,3]) {|a|  a = a}
