1d2111bc472c96426729f752102af399c6bcdfc6
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-14 16:10:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,23 @@ def map(array)
   new = []
   counter = 0 
   while counter < array.length do 
-    yield
+    new.push(yield(array[counter]))
     counter += 1
   end 
   new
 end
 
-map(array) { |i| i * -1 }
+def reduce(array, starting_point = 0)
+  if starting_point != 0 
+    total = starting_point
+    counter = 0 
+  else
+    total = array[0]
+    counter = 1
+  end
+  while counter < array.length do 
+    total = yield(total, (array[counter]))
+    counter += 1 
+  end 
+  total
+end
