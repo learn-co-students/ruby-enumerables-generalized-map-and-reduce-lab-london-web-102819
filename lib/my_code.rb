f355e81e39027c004e29a597ac760d5aa4833339
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-29 16:24:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,53 @@
-def map(element1) 
-  element1.map { |n| n * -1 }
+def map(array)
+  out = []
+  count = 0
+ while (count < array.count) do
+    out.push(yield(array[count]))
+    count+=1
+  end
+ out
 end
 
 
-def map(element2)
-  element2.map {|n| n * 2}
+def reduce(array,*starting)
+  if starting[0]
+    value = starting[0]
+    count = 0
+    else
+    value = array[0]
+    count = 1
+  end
+
+  while count < array.count do
+    value =  yield(value,array[count])
+    count+=1
+  end 
+ value
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def map(element1) 
+#   element1.map { |n| n * -1 }
+#   element1.map {|n| n * 2}
+# end
+
+
+
+
+
 
 
 
